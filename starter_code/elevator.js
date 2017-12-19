d19412551fbaf6fff151d8641332a8c9a373d1c9
Felix Heras <heras.felix@gmail.com>
2017-12-19 17:34:38 +0100

    feat:Iterations 3 and 4

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -5,44 +5,99 @@ class Elevator {
     this.requests   = [];
     this.waitingList = [];
     this.passengers = [];
-    this.direction = "up";
-    this.interval;
+    this.intervalID = "";
+
   }
 
   start() {
-    this.interval = setInterval(() => {
-      this.update();
-    }, 1000);
+    this.updateIntervalID = setInterval(() => this.update(), 1000);
+    }
   }
+
   stop() {
-    clearInterval(this.interval);
+    clearInterval(this.updateIntervalID);
   }
   update() {
-    this.log();
+    if (this.requests.length === 0 && this.waitingList.length === 0) {
+      this.stop();
+    } else if (this.requests.length > 0) {
+    if (this.requests[0] > this.floor) {
+      this.floorUp();
+    } else if (this.requests[0] < this.floor) {
+      this.floorDown();
+    } else if (this.requests[0] === this.floor) {
+      this._passengersLeave(this.passengers[0]);
+    }
+  } else if (this.waitingList.length > 0) {
+    if (this.waitingList[0].originFloor > this.floor) {
+      this.floorUp();
+    } else if (this.waitingList[0].originFloor < this.floor) {
+      this.floorDown();
+    } else if (this.waitingList[0].originFloor === this.floor) {
+      this._passengersEnter(this.waitingList[0]);
+    }
+  }
+  this.log();
    }
-  _passengersEnter() {
-    _passengersEnter(person) {
-      this.passengers.push(person.name);
+
+  _passengersEnter(person) {
+    if (this.waitingList.includes(person)) {
       this.waitingList.splice(this.waitingList.indexOf(person), 1);
+      this.requests.push(person.destinationFloor);
+      this.passengers.push(person);
+      console.log(`${person.name} has entered the elevator`);
    }
-  _passengersLeave() { }
+  _passengersLeave() {
+    if (this.passengers.includes(person)) {
+      this.passengers.splice(this.passengers.indexOf(person), 1);
+      this.requests.splice(0, 1);
+      console.log(`${person.name} has left the elevator`);
+  }
 
   floorUp() {
-    if (this.floor < 10) {
-          this.floor++;
+    if (this.floor === this.MAXFLOOR) {
+      this.floor = this.MAXFLOOR;
+    } else if (this.floor < this.MAXFLOOR) {
+      this.floor += 1;
+      this.direction = "Up";
+    }
   }
 
   floorDown() {
-    if (this.floor > 0) {
-          this.floor--;
-  }
+    if (this.floor === 0) {
+        this.floor = 0;
+      } else if (this.floor > 0) {
+        this.floor -= 1;
+        this.direction = "Down";
+      }
+    }
 
   call(person) {
-    this.requests.push(person);
-    this.waitingList.push(person);
-   }
+    console.log("Elevator Requested");
+   this.waitingList.push(person);
+ }
+
   log() {
-    console.log("Direction: " + this.direction + " | Floor: " + this.floor );
+    console.log("Direction: " + this.direction + "| Floor: " + this.floor);
+   }
+ }
+
+_deleteFromWaitingList (name) {
+    for (let i = 0; i < this.waitingList.length; i++){
+      if (this.waitingList[i].name === name) {
+        this.waitingList.splice(i, 1);
+        return i
+      }
+    }
+  }
+
+  _deleteFromPassengerList (name) {
+    for (let i = 0; i < this.passengers.length; i++){
+      if (this.passengers[i].name === name) {
+        this.passengers.splice(i, 1);
+        return i
+      }
+    }
   }
 }
 
--- starter_code/index.js
+++ starter_code/index.js
@@ -2,13 +2,30 @@ const Elevator = require('./elevator.js');
 const Person = require('./person.js');
 
 var elevator = new Elevator();
+
+const john = new Person("john", 2, 5);
+const mac = new Person("mac", 1, 10);
+const david = new Person("david", 4, 6);
+const helen = new Person("helen", 4, 1);
+const henry = new Person("henry", 9, 4);
+
 elevator.start();
-elevator.stop();
-for (let i = 0; i < 20; i++) {
-  elevator.floorUp();
-  elevator.update();
-}
-for (let i = 0; i < 20; i++) {
-  elevator.floorDown();
-  elevator.update();
-}
+setTimeout(function() {
+  elevator.call(john);
+}, 500);
+
+setTimeout(function() {
+  elevator.call(mac);
+}, 500);
+
+setTimeout(function() {
+  elevator.call(david);
+}, 500);
+
+setTimeout(function() {
+  elevator.call(helen);
+}, 500);
+
+setTimeout(function() {
+  elevator.call(henry);
+}, 500);
